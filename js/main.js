1fca558aae5f46dfad7e474ce98efdcaa5cf0e92
Michael <michaelferrell@Michaels-MacBook-Air.local>
2015-10-10 17:27:13 -0700

    add new art piece

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@
             {'name': 'Burnt Toast','src': 'images/untitled.jpg', 'size': '16" X 20"', 'sold': '', 'tag': 'expressionism'},
             {'name': 'Core Temper','src': 'images/earths_shake.jpg', 'size': '12" X 24"', 'sold': '', 'tag': 'expressionism'},
             {'name': 'When The Colors Meet','src': 'images/when_the_colors_meet.jpg', 'size': '8" X 10"', 'sold': '', 'tag': 'expressionism'},
+            {'name': 'Color Streets','src': 'images/color_streets.jpg', 'size': '12" X 24"', 'sold': '', 'tag': 'expressionism'},
             {'name': 'Fierce River','src': 'images/violent_waterfall.jpg', 'size': '18" X 24"', 'sold': '', 'tag': 'expressionism'},
             {'name': 'Untitled','src': 'images/untitled_3.jpg', 'size': '16" X 16"', 'sold': '', 'tag': 'slang'},
             {'name': 'Slang','src': 'images/untitled_2.jpg', 'size': '11" X 14"', 'sold': 'sold', 'tag': 'slang'},
